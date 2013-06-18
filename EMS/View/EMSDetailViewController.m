9f796b5b15db014c1d7784795630f1aa882e1daa
chrsearl <chris.searle@finn.no>
2013-06-18 09:18:18 +0200

    Log share completion

--- EMS/View/EMSDetailViewController.m
+++ EMS/View/EMSDetailViewController.m
@@ -25,7 +25,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+
 - (void)share:(id)sender {
+    // More info - http://blogs.captechconsulting.com/blog/steven-beyers/cocoaconf-dc-recap-sharing-uiactivityviewcontroller
+
     NSString *shareString = [NSString stringWithFormat:@"%@ - %@", [[self.session valueForKey:@"conference"] valueForKey:@"name"], [self.session valueForKey:@"title"]];
     
     CLS_LOG(@"About to share for %@", shareString);
@@ -34,8 +37,21 @@
     // TODO - web URL?
     NSURL *shareUrl = [NSURL URLWithString:@"http://www.java.no"];
     NSArray *activityItems = [NSArray arrayWithObjects:shareString, /*shareImage, */shareUrl, nil];
-    UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:nil];
+    
+    UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:activityItems
+                                                                                         applicationActivities:nil];
+    
+    activityViewController.excludedActivityTypes = @[UIActivityTypePrint,
+                                                     UIActivityTypeCopyToPasteboard,
+                                                     UIActivityTypeAssignToContact,
+                                                     UIActivityTypeSaveToCameraRoll];
+    
+    [activityViewController setCompletionHandler:^(NSString *activityType, BOOL completed) {
+        CLS_LOG(@"Sharing of %@ via %@ - completed %d", shareString, activityType, completed);
+    }];
+    
     activityViewController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+    
     [self presentViewController:activityViewController animated:YES completion:nil];
 }
 
