dbc0a7407e0c58bb79c1b97c391bb9be6ccd3950
Chris Searle <chris@chrissearle.org>
2013-06-20 11:11:56 +0200

    Add share to calendar

--- EMS/View/EMSDetailViewController.m
+++ EMS/View/EMSDetailViewController.m
@@ -3,6 +3,7 @@
 //
 
 #import <CommonCrypto/CommonDigest.h>
+#import <EventKit/EventKit.h>
 
 #import "EMSDetailViewController.h"
 
@@ -13,6 +14,10 @@
 #import "Session.h"
 #import "Speaker.h"
 #import "Keyword.h"
+#import "Room.h"
+
+#import "NHCalendarActivity.h"
+#import "NHCalendarEvent.h"
 
 @interface EMSDetailViewController ()
 
@@ -94,6 +99,64 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSString *)buildCalendarNotes {
+    NSMutableString *result = [[NSMutableString alloc] init];
+    
+    [result appendString:@"Details\n\n"];
+    [result appendString:self.session.body];
+    
+    if (self.session.keywords != nil && [self.session.keywords count] > 0) {
+        [result appendString:@"\n\nKeywords\n\n"];
+        
+        NSMutableArray *listItems = [[NSMutableArray alloc] init];
+            
+        [self.session.keywords enumerateObjectsUsingBlock:^(id obj, BOOL *stop) {
+            Keyword *keyword = (Keyword *)obj;
+                
+            [listItems addObject:keyword.name];
+        }];
+            
+        [result appendFormat:@"* %@\n\n", [[listItems sortedArrayUsingSelector:@selector(compare:)] componentsJoinedByString:@"\n* "]];
+    }
+    
+    if ([self.session.speakers count] > 0) {
+        [result appendString:@"\n\nSpeakers\n\n"];
+        
+        [self.session.speakers enumerateObjectsUsingBlock:^(id obj, BOOL *stop) {
+            Speaker *speaker = (Speaker *)obj;
+            
+            [result appendString:speaker.name];
+            if (speaker.bio != nil) {
+                [result appendString:@"\n\n"];
+                [result appendString:speaker.bio];
+            }
+            [result appendString:@"\n\n"];
+        }];
+    }
+    
+    return [NSString stringWithString:result];
+}
+
+- (NHCalendarEvent *)createCalendarEvent
+{
+    NHCalendarEvent *calendarEvent = [[NHCalendarEvent alloc] init];
+    
+    calendarEvent.title = [NSString stringWithFormat:@"%@ - %@", self.session.conference.name, self.session.title];
+    calendarEvent.location = self.session.room.name;
+    calendarEvent.notes = [self buildCalendarNotes];
+    calendarEvent.startDate = [self dateForDate:self.session.slot.start];
+    calendarEvent.endDate = [self dateForDate:self.session.slot.end];
+    calendarEvent.allDay = NO;
+    
+    // Add alarm
+    NSArray *alarms = @[[EKAlarm alarmWithRelativeOffset:- 60.0f * 5.0f]];
+    
+    calendarEvent.alarms = alarms;
+    
+    CLS_LOG(@"Created calendar event %@", calendarEvent);
+    
+    return calendarEvent;
+}
 
 - (void)share:(id)sender {
     // More info - http://blogs.captechconsulting.com/blog/steven-beyers/cocoaconf-dc-recap-sharing-uiactivityviewcontroller
@@ -102,13 +165,15 @@
     
     CLS_LOG(@"About to share for %@", shareString);
     
-//TODO - image?    UIImage *shareImage = [UIImage imageNamed:@"captech-logo.jpg"];
     // TODO - web URL?
-    NSURL *shareUrl = [NSURL URLWithString:@"http://www.java.no"];
-    NSArray *activityItems = [NSArray arrayWithObjects:shareString, /*shareImage, */shareUrl, nil];
+    // NSURL *shareUrl = [NSURL URLWithString:@"http://www.java.no"];
     
+    NSArray *activityItems = [NSArray arrayWithObjects:shareString, /*shareUrl, */ [self createCalendarEvent], nil];
+    
+    NSArray *activities = @[[[NHCalendarActivity alloc] init]];
+
     __block UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:activityItems
-                                                                                         applicationActivities:nil];
+                                                                                         applicationActivities:activities];
     
     activityViewController.excludedActivityTypes = @[UIActivityTypePrint,
                                                      UIActivityTypeCopyToPasteboard,
@@ -267,6 +332,26 @@
         [output appendFormat:@"%02x", digest[i]];
     
     return  output;
+}
+
+- (NSDate *)dateForDate:(NSDate *)date {
+#ifdef USE_TEST_DATE
+    CLS_LOG(@"WARNING - RUNNING IN USE_TEST_DATE mode");
+    
+	// In debug mode we will use the current day but always the start time of the slot. Otherwise we couldn't test until JZ started ;)
+	NSCalendar *calendar = [NSCalendar currentCalendar];
+    
+	NSDateComponents *timeComp = [calendar components:NSHourCalendarUnit|NSMinuteCalendarUnit fromDate:date];
+	NSDateComponents *dateComp = [calendar components:NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit fromDate:[[NSDate alloc] init]];
+    
+    NSDateFormatter *inputFormatter = [[NSDateFormatter alloc] init];
+    [inputFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss ZZ"];
+    [inputFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+    
+	return [inputFormatter dateFromString:[NSString stringWithFormat:@"%04d-%02d-%02d %02d:%02d:00 +0200", [dateComp year], [dateComp month], [dateComp day], [timeComp hour], [timeComp minute]]];
+#else
+    return date;
+#endif
 }
 
 @end
