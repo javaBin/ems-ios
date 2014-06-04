c33de5baf849d9f075d971f6a21aba7bd75016bf
Ronnie Nessa <ronnie.nessa@gmail.com>
2014-06-04 20:34:23 +0200

    Moved private properties and methods to private class extension in implementation file. This makes the public API clearer.

--- EMS/View/EMSMainViewController.h
+++ EMS/View/EMSMainViewController.h
@@ -12,31 +12,8 @@
 
 #import "EMSModel.h"
 
-@interface EMSMainViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate, EMSRetrieverDelegate, NSFetchedResultsControllerDelegate, UISearchBarDelegate, EMSSearchViewDelegate>
-
-@property(nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
-@property(nonatomic, strong) EMSRetriever *retriever;
-
-@property(nonatomic, assign) BOOL retrievingSlots;
-@property(nonatomic, assign) BOOL retrievingRooms;
-
-@property(nonatomic, assign) BOOL filterFavourites;
-@property(nonatomic, assign) BOOL filterTime;
-
-@property(nonatomic, strong) EMSAdvancedSearch *advancedSearch;
-
-@property(nonatomic, strong) IBOutlet UISearchBar *search;
-@property(nonatomic, strong) IBOutlet UIBarButtonItem *advancedSearchButton;
-
-@property(nonatomic, strong) IBOutlet UIView *footer;
-@property(nonatomic, strong) IBOutlet UILabel *footerLabel;
-
-- (IBAction)toggleFavourite:(id)sender;
-
-- (IBAction)segmentChanged:(id)sender;
+@interface EMSMainViewController : UITableViewController
 
 - (void)pushDetailViewForHref:(NSString *)href;
-
-- (IBAction)back:(UIStoryboardSegue *)segue;
 
 @end
--- EMS/View/EMSMainViewController.m
+++ EMS/View/EMSMainViewController.m
@@ -17,8 +17,33 @@
 #import "Speaker.h"
 #import "Room.h"
 
-@interface EMSMainViewController () <UISplitViewControllerDelegate>
+@interface EMSMainViewController () <UISplitViewControllerDelegate,UITableViewDataSource, UITableViewDelegate, EMSRetrieverDelegate, NSFetchedResultsControllerDelegate, UISearchBarDelegate, EMSSearchViewDelegate>
+
 @property(nonatomic, strong) EMSDetailViewController *detailViewController;
+
+@property(nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
+@property(nonatomic, strong) EMSRetriever *retriever;
+
+@property(nonatomic, assign) BOOL retrievingSlots;
+@property(nonatomic, assign) BOOL retrievingRooms;
+
+@property(nonatomic, assign) BOOL filterFavourites;
+@property(nonatomic, assign) BOOL filterTime;
+
+@property(nonatomic, strong) EMSAdvancedSearch *advancedSearch;
+
+@property(nonatomic, strong) IBOutlet UISearchBar *search;
+@property(nonatomic, strong) IBOutlet UIBarButtonItem *advancedSearchButton;
+
+@property(nonatomic, strong) IBOutlet UIView *footer;
+@property(nonatomic, strong) IBOutlet UILabel *footerLabel;
+
+- (IBAction)toggleFavourite:(id)sender;
+
+- (IBAction)segmentChanged:(id)sender;
+
+- (IBAction)back:(UIStoryboardSegue *)segue;
+
 @end
 
 @implementation EMSMainViewController
