9d1be2d669ee13398506be2798319c46b10ae1b1
Ronnie Nessa <ronnie.nessa@gmail.com>
2014-06-08 18:36:05 +0200

    Moved all non-public properties and methods for EMSDetailViewController to the private extension class. This makes the API a little bit easier to understand.

--- EMS/View/EMSDetailViewController.h
+++ EMS/View/EMSDetailViewController.h
@@ -7,41 +7,13 @@
 
 #import "Session.h"
 
-@interface EMSDetailViewController : UIViewController <EMSRetrieverDelegate, UITableViewDataSource, UITableViewDelegate, UIGestureRecognizerDelegate>
+@interface EMSDetailViewController : UIViewController 
 
 @property(nonatomic, strong) Session *session;
-@property(nonatomic, strong) IBOutlet UIWebView *webView;
-
-@property(nonatomic, strong) IBOutlet UILabel *titleLabel;
-
-@property(nonatomic, strong) IBOutlet UIButton *button;
-
-@property(nonatomic, strong) IBOutlet UITableView *tableView;
-
-@property(nonatomic, strong) NSDictionary *cachedSpeakerBios;
-
-@property(nonatomic, strong) IBOutlet UIBarButtonItem *previousSectionButton;
-@property(nonatomic, strong) IBOutlet UIBarButtonItem *nextSectionButton;
-@property(nonatomic, strong) IBOutlet UIBarButtonItem *previousSessionButton;
-@property(nonatomic, strong) IBOutlet UIBarButtonItem *nextSessionButton;
-@property(nonatomic, strong) IBOutlet UIBarButtonItem *shareButton;
-
 @property(nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
 @property(nonatomic, strong) NSIndexPath *indexPath;
 
-- (IBAction)share:(id)sender;
-
-- (IBAction)toggleFavourite:(id)sender;
 
 - (void)refreshFavourite;
-
-- (IBAction)movePreviousSection:(id)sender;
-
-- (IBAction)moveNextSection:(id)sender;
-
-- (IBAction)movePreviousSession:(id)sender;
-
-- (IBAction)moveNextSession:(id)sender;
-
 
 @end
--- EMS/View/EMSDetailViewController.m
+++ EMS/View/EMSDetailViewController.m
@@ -21,9 +21,44 @@
 
 #import "NHCalendarActivity.h"
 
-@interface EMSDetailViewController () <UIPopoverControllerDelegate>
+@interface EMSDetailViewController () <UIPopoverControllerDelegate,EMSRetrieverDelegate, UITableViewDataSource, UITableViewDelegate, UIGestureRecognizerDelegate>
+
 @property(nonatomic) UIPopoverController *sharePopoverController;
 @property(nonatomic) NSArray *parts;
+
+
+@property(nonatomic, strong) IBOutlet UIWebView *webView;
+
+@property(nonatomic, strong) IBOutlet UILabel *titleLabel;
+
+@property(nonatomic, strong) IBOutlet UIButton *button;
+
+@property(nonatomic, strong) IBOutlet UITableView *tableView;
+
+@property(nonatomic, strong) NSDictionary *cachedSpeakerBios;
+
+@property(nonatomic, strong) IBOutlet UIBarButtonItem *previousSectionButton;
+@property(nonatomic, strong) IBOutlet UIBarButtonItem *nextSectionButton;
+@property(nonatomic, strong) IBOutlet UIBarButtonItem *previousSessionButton;
+@property(nonatomic, strong) IBOutlet UIBarButtonItem *nextSessionButton;
+@property(nonatomic, strong) IBOutlet UIBarButtonItem *shareButton;
+
+
+
+
+- (IBAction)share:(id)sender;
+
+- (IBAction)toggleFavourite:(id)sender;
+
+
+- (IBAction)movePreviousSection:(id)sender;
+
+- (IBAction)moveNextSection:(id)sender;
+
+- (IBAction)movePreviousSession:(id)sender;
+
+- (IBAction)moveNextSession:(id)sender;
+
 
 @end
 
